338a97f0ac5ee0226920bb0ec5ba636e2f243e0c
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-02-26 16:36:57 +0900

    Ask for permission and create Alert.

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import Loading from "./Loading";
+import { Alert } from "react-native";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+    } catch (error) {
+      Alert.alert("Can't find you", "So sad");
+    }
   };
   componentDidMount() {
     this.getLocation();
   }
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
